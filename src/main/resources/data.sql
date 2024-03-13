b0103dd54eb3fceff9221ea5b72eeba27c9ef607
Julian Mojico <jmojico@gmail.com>
2024-03-12 21:39:13 -0300

    h2 db embedded file persistance

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS Products (
+                                        id INT,
+                                        name VARCHAR(255),
+    status VARCHAR(50)
+    );
+
+INSERT INTO Products (id, name, status) VALUES (1, 'Laptop', 1);
+INSERT INTO Products (id, name, status) VALUES (1, 'Gamer Mouse', 0);
